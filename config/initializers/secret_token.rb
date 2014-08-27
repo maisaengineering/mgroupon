2b20b42af1315acf621ff78ee200cd988c51f1e3
maisaengineering <contact@maisasolutions.com>
2014-08-27 15:42:53 +0530

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mgroupon::Application.config.secret_key_base = '5b66764885647d2506c7c5f887b3176bcd1a99d19f48760be93c8eb397e60dcbe166b4ca604b51d3c649a262cdbc85966cc19c82608cd761f6a5c7aaa26fd1f6'
